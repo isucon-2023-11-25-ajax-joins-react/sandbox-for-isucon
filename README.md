d7417cf503bd1a3c66c278aea2bb9af1c11c34b8
Ochi-Shoichi5539 <dekamintv@gmail.com>
2023-11-25 09:46:28 +0900

    isucon用のsandboxを作成する

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # sandbox-for-isucon
 isuconのサンドボックス
+
+## 環境構築
+```bash
+$ docker compose -f infra/docker-compose.yml build
+```
+### appの環境構築(gemの導入)
+```bash
+$ bin/exec-bash app
+$ bundle install
+```
+
+## mysql2の実行
+```ruby
+client = Database::Client.connect_db
+client.xquery('show databases;')
+```
+
+## tips
+```bash
+$ bin/exec-bash app
+```
+
+```bash
+$ bin/exec-bash database
+```
---
+++ app/lib/database/client.rb
@@ -0,0 +1,20 @@
+require 'mysql2'
+require 'mysql2-cs-bind'
+
+module Database
+  class Client
+    def self.connect_db
+      # Ref: https://github.com/brianmario/mysql2#usage
+      Mysql2::Client.new(
+        # Docker Composeのコンテナ名をホストとして指定する（docker composeを用いると、コンテナ名がホスト名になる。）
+        host: 'database',
+        username: 'root',
+        password: 'root',
+        database: 'demo'
+      )
+    end
+  end
+end
+
+
+binding.irb
